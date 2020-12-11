e42ec702444e7f6b8997d0c65cfb15ccff6a6e62
davidkandy <davidkandy98@gmail.com>
2020-12-11 11:28:30 +0100

    Db Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Configuration;
 using System.Windows;
+using System.Data.SqlClient;
 
 namespace MyDiary
 {
@@ -8,17 +9,18 @@ namespace MyDiary
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlConnection;
         public MainWindow()
         {
             InitializeComponent();
 
             string connectionString = ConfigurationManager.ConnectionStrings["MyDiary.Properties.Settings.MyDiaryDBConnectionString"].ConnectionString;
+
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
-            
         }
     }
 }
