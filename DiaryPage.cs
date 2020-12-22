e16b6678e6fd733f2f7c5c4fe2da76247418c438
davidkandy <davidkandy98@gmail.com>
2020-12-22 16:59:57 +0100

    ....

---
+++ DiaryPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyDiary
+{
+    public class DiaryPage
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastUpdated { get; set; }
+    }
+}
